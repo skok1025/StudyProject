8e8b9442ee1c999b5c0fd7a83ad9a69991504234
shkim <skok1025>
2021-10-17 18:20:31 +0900

    크롤링

--- src/main/java/com/skok1025/study/craw/rank_crawer.py
+++ src/main/java/com/skok1025/study/craw/rank_crawer.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 type = sys.argv[1]
 
+# 영화 순위
 def movieRank():
     crawLink = 'https://movie.naver.com/movie/sdb/rank/rmovie.nhn'
     request = Request(crawLink)
@@ -33,6 +34,7 @@ def movieRank():
     print(json.dumps(movieRank, ensure_ascii=False))
     # print("==============================")
 
+# 프리미어리그 순위
 def PremierRank():
     request = Request('https://www.premierleague.com/tables?co=1&se=42&mw=-1&ha=-1')
     response = urlopen(request)
@@ -47,14 +49,70 @@ def PremierRank():
         data = {
             'rank': index + 1,
             'name': tr['data-filtered-table-row-name'],
-            'points': tr.find('td', class_='points').text
+            'points': tr.find('td', class_='points').text,
+            'badge-image' : tr.find('img', class_='badge-image')['src']
         }
 
         print(json.dumps(data, ensure_ascii=False))
 
     print("==============================")
 
-if type == 'movie' :
+# 뉴스 순위
+def NewsRank():
+    request = Request('https://news.daum.net/ranking/popular')
+    response = urlopen(request)
+    html = response.read().decode('UTF-8')
+    # print(html)
+
+    bs = BeautifulSoup(html, 'html.parser')
+    # print(bs.prettify())
+    newsList = bs.findAll('li', attrs={'data-tiara-layer': 'news_list'})
+    # print(divs)
+    for index, news in enumerate(newsList):
+        newsItem = news.find('a', class_='link_txt')
+        newsImg = news.find('img', class_='thumb_g')
+
+        data = {
+            'rank': index + 1,
+            'title': newsItem.text,
+            'href' : newsItem['href'],
+            'img' : newsImg['src']
+        }
+
+        print(json.dumps(data, ensure_ascii=False))
+
+    print("==============================")
+
+# 뮤지컬 순위
+def MusicalRank():
+    request = Request('http://ticket.interpark.com/Contents/Ranking')
+    response = urlopen(request)
+    html = response.read().decode('UTF-8')
+    # print(html)
+
+    bs = BeautifulSoup(html, 'html.parser')
+    # print(bs.prettify())
+    div = bs.find('div', class_='rankingGenre genre1').findAll('a', attrs={'class': 'prdName'})
+
+    for index, tr in enumerate(div):
+        data = {
+            'rank': index + 1,
+            'name': tr.next
+        }
+        print(json.dumps(data, ensure_ascii=False))
+
+    print("==============================")
+
+
+if type == 'movie':
     movieRank()
 elif type == 'premier':
     PremierRank()
+elif type == 'news':
+    NewsRank()
+elif type == 'musical':
+    MusicalRank()
+
+# 배달 순위
+# 여행지 순위
+# 립스틱 순위
